56d08b0eee2e674c2869cc66db6fe10e0eee6972
shahabuddin khan <smusalman280@rku.ac.in>
2025-09-21 16:39:55 +0545

    Remove old files and add new project code

--- hello/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('abcd.urls')),  # route home page to our hello app
-]
